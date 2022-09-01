b16cddb84b27a5d1ff4df3f9bc7216a23cfe5f24
Meliodasbot <112660595+Meliodasbot@users.noreply.github.com>
2022-09-01 22:39:34 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['01126554338']
+global.premium = ['01126554338']
+global.ownernomer = '01126554338'
 global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.botname = 'Meliodas'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
